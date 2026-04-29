07ac5211247c5f2738de69862bf1ee3c12b5f741
AlexDuhanin <alexduh@yandex.ru>
2026-04-29 14:45:37 +0300

    Version 1

---
+++ gcalendar.py
@@ -0,0 +1,123 @@
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+calendar_id = "e16086764890938807f37f2df9396ce1d3d52d39aa65f909e69abf23728ed097@group.calendar.google.com"
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly",
+          "https://www.googleapis.com/auth/calendar"]
+
+
+class Gcalendar:
+
+    def __init__(self, calendar_id=calendar_id):
+        self.calendar_id = calendar_id
+
+    def autorize(self):
+        creds = None
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+        return creds
+
+    def build(self):
+        creds = self.autorize()
+        service = build("calendar", "v3", credentials=creds)
+        return service
+
+    def events_list(self, count=10, name=None):
+        """Получаем count число событий из календаря, у которых в названии есть name"""
+        service = self.build()
+        now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+        events_result = (
+            # Сортировку по name можно сделать и здесь.
+            # https://developers.google.com/workspace/calendar/api/v3/reference/events/list#:~:text=q-,string,-%D0%9F%D0%BE%D0%B8%D1%81%D0%BA%20%D0%BF%D0%BE%20%D1%81%D0%B2%D0%BE%D0%B1%D0%BE%D0%B4%D0%BD%D0%BE%D0%BC%D1%83
+            service.events().list(
+                calendarId=self.calendar_id,
+                timeMin=now,
+                maxResults=count,
+                singleEvents=True,
+                orderBy="startTime",
+                q=name  # на всякий случай еще здесь сортируем по name
+            )
+            .execute()
+        )
+        print('events', events_result)
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            return
+
+        if name:
+            # Сортировка
+            events = [event for event in events if name in event["summary"]]
+
+        return events
+
+        # print(events)
+        # for event in events:
+        #     start = event["start"].get("dateTime", event["start"].get("date"))
+        # print(start, event["summary"])
+        # print('id', event["id"])
+        # test_event_id = event["id"]
+
+    def event_get(self, event_id):
+        """Получаем событие по его уникальному id"""
+        service = self.build()
+        body = (
+            service.events().get(
+                calendarId=calendar_id,
+                eventId=event_id
+            )
+            .execute()
+        )
+        if not body:
+            return
+
+        return body
+
+    def event_update(self, event_id, summary=None, color_id=None,
+                     start: datetime = None, end: datetime = None,
+                     description=None):
+        """Обновляем событие, изменяя ему необходимые данные
+        Можно поменять: название (summary), дату (start/end), цвет (colorId)"""
+        service = self.build()
+        body = self.event_get(event_id=event_id)
+        if not body: return None
+
+        if summary: body["summary"] = summary
+        if color_id: body["colorId"] = color_id
+        if start: body["start"] = start
+        if end: body["end"] = end
+        if description: body["description"] = description
+
+        result = (
+            service.events().update(
+                calendarId=calendar_id,
+                eventId=event_id,
+                body=body
+            ).execute()
+        )
+        print('result', result)
+
+# calendar = Gcalendar()
+
+# events = calendar.events_list(count=1, name="ABC")
+# print(events)
+# event = events[0]
+# calendar.event_update(event["id"], summary="QWERTY")
---
+++ main.py
@@ -0,0 +1,192 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+    CallbackQueryHandler,
+    ConversationHandler
+)
+from gcalendar import Gcalendar
+import datetime as dt
+
+calendar = Gcalendar()
+
+TOKEN = "8541968958:AAEwHG8R16l2eu_6-MdiLdkdJ-IgGmwUzbE"  # токен бота
+(VOICE, START_VOICE, FREE_SLOT,
+ NAME, CONTACT, END,
+ GUITAR,
+ ACCEPT, CANCEL, SUCCES) = range(0, 10)
+
+ADMIN = 5181963292
+
+
+def time_to_dt(time: str):
+    tm = dt.datetime.fromisoformat(time)
+    return tm
+
+
+def time_to_str(time: dt.datetime):
+    pass
+
+
+def human_time(time: dt.datetime):
+    tm = dt.datetime.strftime(time, "%d.%m.%Y | %H:%M:%S")
+    return tm
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text = "Приветствие.\nВыберите направление:"
+    keyboard = [[InlineKeyboardButton("Вокал", callback_data=str(VOICE))],
+                [InlineKeyboardButton("Гитара", callback_data=str(GUITAR))]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(text=text, reply_markup=reply_markup)
+
+
+async def voice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_data = context.user_data
+    text = "Информация о занятиях вокала и пр."
+    query = update.callback_query
+
+    # Запрос к календарю "Свободный слот (вокал)"
+    user_data["main_choose"] = "events_voice"
+    events = user_data["events_voice"] = calendar.events_list(name="Свободный слот (вокал)")
+    if not events:
+        await query.edit_message_text(text="Свободных мест нет")
+        return ConversationHandler.END
+
+    keyboard = []
+    for i, event in enumerate(events):
+        start = event["start"]
+        tm = human_time(time_to_dt(start["dateTime"]))
+        keyboard.append([InlineKeyboardButton(f"{tm}", callback_data=f"{VOICE}-{i}")])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.answer()
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
+    return NAME
+
+
+async def guitar(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_data = context.user_data
+    text = "Информация о занятиях гитары и пр."
+    query = update.callback_query
+
+    # Запрос к календарю "Свободный слот (гитара)"
+    user_data["main_choose"] = "events_guitar"
+    events = user_data["events_guitar"] = calendar.events_list(name="Свободный слот (гитара)")
+    if not events:
+        await query.edit_message_text(text="Свободных мест нет")
+        return ConversationHandler.END
+
+    keyboard = []
+    for i, event in enumerate(events):
+        start = event["start"]
+        tm = human_time(time_to_dt(start["dateTime"]))
+        keyboard.append([InlineKeyboardButton(f"{tm}", callback_data=f"{GUITAR}-{i}")])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.answer()
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
+    return NAME
+
+
+async def name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_data = context.user_data
+    # message = update.effective_message.text
+    text = "Введите свое имя:"
+    query = update.callback_query
+    await query.answer()
+    event_choose = int(query.data.split("-")[1])
+    user_data["event_choose"] = event_choose
+    await update.effective_user.send_message(text=text)
+    return CONTACT
+
+
+async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_data = context.user_data
+    message = update.effective_message.text  # Принять имя
+    user_data["name"] = message
+    text = "Введите свое контакт:"
+    await update.message.reply_text(text=text)
+    return ACCEPT
+
+
+async def accept(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_data = context.user_data
+    message = update.effective_message.text  # Принять контакт
+    user_data["contact"] = message
+    text = "Подтвердите бронь"
+    keyboard = [[InlineKeyboardButton("Да", callback_data=str(ACCEPT))],
+                [InlineKeyboardButton("Отмена", callback_data=str(CANCEL))]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(text=text, reply_markup=reply_markup)
+    return SUCCES  # succes
+
+
+async def succes(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_data = context.user_data
+    # Принять ACCEPT
+    text = "Запись успешно завершена"
+    query = update.callback_query
+    await query.answer()
+    await update.effective_user.send_message(text=text)
+
+    event_choose = user_data[user_data["main_choose"]][user_data["event_choose"]]
+    event_id = event_choose["id"]
+    calendar.event_update(event_id=event_id, summary=user_data["name"],
+                          color_id=3, description=f"Контакт: {user_data["contact"]}")
+    # Информируем админа о новой записи
+    tm = human_time(time_to_dt(event_choose["start"]["dateTime"]))
+    text = f"Новая запись:\nВремя записи: {tm}\nКонтакт: {user_data["contact"]}"
+    await context.bot.send_message(chat_id=ADMIN,
+                                   text=text)
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text = "Запись отменена"
+    query = update.callback_query
+    await query.answer()
+    await update.effective_user.send_message(text=text)
+    return ConversationHandler.END
+
+
+# proxy_url = 'http://username:password@host:port' # или socks5://
+# .proxy_url(proxy_url).get_updates_proxy_url(proxy_url)
+# app = ApplicationBuilder.token(TOKEN).build()
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+conv_handler_voice = ConversationHandler(
+    entry_points=[CallbackQueryHandler(voice, pattern=f"^{VOICE}$")],
+    states={
+        NAME: [CallbackQueryHandler(name, pattern=f"^({VOICE})-")],
+        CONTACT: [MessageHandler(filters.TEXT & ~filters.COMMAND, contact)],
+        ACCEPT: [MessageHandler(filters.TEXT & ~filters.COMMAND, accept)],
+        SUCCES: [CallbackQueryHandler(succes, pattern=str(ACCEPT)),
+                 CallbackQueryHandler(cancel, pattern=str(CANCEL))],
+    },
+    fallbacks=[CommandHandler("start", start)],
+)
+
+conv_handler_guitar = ConversationHandler(
+    entry_points=[CallbackQueryHandler(guitar, pattern=f"^{GUITAR}$")],
+    states={
+        NAME: [CallbackQueryHandler(name, pattern=f"^({GUITAR})-")],
+        CONTACT: [MessageHandler(filters.TEXT & ~filters.COMMAND, contact)],
+        ACCEPT: [MessageHandler(filters.TEXT & ~filters.COMMAND, accept)],
+        SUCCES: [CallbackQueryHandler(succes, pattern=str(ACCEPT)),
+                 CallbackQueryHandler(cancel, pattern=str(CANCEL))],
+    },
+    fallbacks=[CommandHandler("start", start)],
+)
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(conv_handler_voice)
+app.add_handler(conv_handler_guitar)
+
+# app.add_handler(CommandHandler("hello", hello))
+# app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+app.run_polling()
